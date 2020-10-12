e4fa7a0e791a117007b75fc69747b06e035061c7
Logz11 <reinkell@gmatc.matc.edu>
2020-10-11 22:42:09 -0500

    Added error and loading states

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -16,7 +16,9 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
-  },
+    minHeight: 260,
+    minWidth: 214,
+    },
   root: {
     display: "flex",
     alignItems: "center",
@@ -26,75 +28,98 @@ const useStyles = makeStyles((theme) => ({
   formContainer: {
     width: "fit-content",
   },
+  error: {
+    color: "red",
+    fontWeight: "bold",
+  },
 }));
 
 const App = () => {
   const [users, setUsers] = useState(null);
+  const [user, setUser] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [authorization, setAuth] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const classes = useStyles();
 
   useEffect(() => {
-    axios.get('/users')
+    if(user) {
+      setAuth(true);
+      setLoading(false);
+    }
+  }, [user]);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    axios.get(`/users`)
       .then((response) => {
-        setUsers(response?.data)
-        console.log({response})
+        let userFound = (response?.data.filter((user) => user?.username === username && user?.password === password
+        ))
+        if (userFound.length > 0){
+          console.log({response})
+          setUser(userFound);
+        } else if(response?.status === 200 && userFound.length <= 0) {
+          console.log({response})
+          setLoading(false);
+          setError(true);
+        }
       })
       .catch((error) => {
         console.error({error});
       })
-  }, []);
-
-  const handleSubmit = async () => {
-    setLoading(true);
-    axios.get('/users', {
-      username: username,
-      password: password,
-    })
   }
 
-  console.log({username})
-  console.log({password})
-  console.log({users})
-
+  console.log({user})
+  console.log({authorization})
 
   return (
     <div className="App">
     <div className={classes.root}>
     <div className={classes.formContainer}>
-    <Paper
-      classes={{
-        root: classes.paper,
-      }}
-    >
-    <Grid container direction="column" justify="center" alignItems="center" spacing={3}>
-    <Grid item>
-      <Typography variant="h3" component="h1">
-        Login
-      </Typography>
-    </Grid>
-      <Grid item>
-        <TextField
-          label="Username"
-          onChange={(event) => setUsername(event.target.value)}>
-        </TextField>
-      </Grid>
-      <Grid item>
-        <TextField 
-          label="Password"
-          onChange={(event) => setPassword(event.target.value)}>
-        </TextField>
-      </Grid>
-      <Grid item>
-      <Button color="primary" variant="contained" onClick={() => handleSubmit()}>
-        SUBMIT
-      </Button>
-      </Grid>
-    </Grid>
-    </Paper>
+    {!authorization && 
+      <Paper
+        classes={{
+          root: classes.paper,
+        }}
+      >
+        <Grid container direction="column" justify="center" alignItems="center" spacing={3}>
+        <Grid item>
+          <Typography variant="h3" component="h1">
+            Login
+          </Typography>
+        </Grid>
+        <>
+          {error ? <Typography classes={{root: classes.error}}>Incorrect username or password.</Typography> : null}
+        </>
+          {loading
+          ? <Grid item>
+              <img src="/images/25.gif" />
+            </Grid>
+          : <><Grid item>
+            <TextField
+              label="Username"
+              onChange={(event) => setUsername(event.target.value)}>
+            </TextField>
+          </Grid>
+          <Grid item>
+            <TextField 
+              label="Password"
+              onChange={(event) => setPassword(event.target.value)}>
+            </TextField>
+          </Grid></>}
+          <Grid item>
+          <Button color="primary" variant="contained" onClick={() => handleSubmit()} disabled={Boolean(loading)}>
+            SUBMIT
+          </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+      ||
+      <Typography>Welcome {user[0]?.username}!</Typography>
+    }
     </div>
     </div>
       {users && users.map((user) => {
